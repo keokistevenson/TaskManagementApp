54242358c4faeccb327c6b7fe37f7932e3c6f068
keokistevenson <keokistevenson@gmail.com>
2026-03-22 20:15:39 -0400

    Worked on task status. Probably overdoing it. I'm sure it needs more work.

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const txtTaskName = document.getElementById("txtTaskName"),
     ddlStatus = document.getElementById("ddlStatus"),
     calDeadline = document.getElementById("calDeadline"),
     btnAddTask = document.getElementById("btnAddTask");
+
+// I need to revisit this later. I'm already complicating things.
+const taskStatus = {
+    In_Progress: "In Progress",
+    Completed: "Completed",  // I think we are mixing terms. A task can be overdue and completed, or it can be in progres and overdue.
+    Overdue: "Overdue"
+};
 class Task {
     constructor(name, deadline = null) {  // I hope I don't regret trying to use object oriented concepts. 
         let date;
@@ -11,30 +18,44 @@ class Task {
         if (deadline === null) {
             date = new Date();
             date.setDate(date.getDate() + 1); // default = tomorrow
-            date.setHours(0, 0, 0, 0);  // Set to tomorrow's time to midnight.
         } else {
+            date = new Date(deadline);  // I will not address time.
 
             if (isNaN(deadline.getTime())) {
                 throw new Error("Invalid deadline");
             }
-            date = new Date(deadline);  // I will not address time.
         }
 
-        if (isNaN(date.getTime())) {
-            throw new Error("Invalid deadline");
-        }
+        date.setHours(0, 0, 0, 0);  // Ignores time for now. Considers date only.
 
         this.name = name;
         this.deadline = date;
+        this.status = taskStatus.In_Progress;
+    }
+
+    getStatus() {
+        return this.status;  // Tried to encapsulate Status.
+    }
+
+    markComplete() {
+        this.status = taskStatus.Completed;
+    }
+
+    isOverdue() {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        this.status = taskStatus.Overdue;
+
+        return today > this.deadline;
+    }
+
+    isDueToday() {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        return today.getTime() === this.deadline.getTime(); // as long as its the same day its due.
     }
 }
-
-
-const taskStatus = {
-    progress: "In Progress",
-    completed: "Completed",
-    overdue: "Overdue"
-};
 
 
 
